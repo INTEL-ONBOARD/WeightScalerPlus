20b943f56f15a0dad09e2c4bd4971fbb3e9b2caa
Office <Office@DESKTOP-3680TA8>
2025-03-20 05:09:18 -0700

    fixes: small bug fixes

--- WeightMaster/Interfaces/MainWindow.xaml.cs
+++ WeightMaster/Interfaces/MainWindow.xaml.cs
@@ -315,11 +315,11 @@ namespace WeightMaster
 
 
 
-        public async Task testExecution()
-        {
+        //public async Task testExecution()
+        //{
 
-            await _consoleHandler.GetLineMasterAsync();
-        }
+        //    await _consoleHandler.GetLineMasterAsync();
+        //}
 
         //made this global to used on the close event
         private CustomerWindow customerWindow;
@@ -365,7 +365,6 @@ namespace WeightMaster
 
         private async void LoginButtonClick(object sender, RoutedEventArgs e)
         {
-            await _consoleHandler.GetLineMasterAsync();
             //checks db records whether they exists
             await _consoleHandler.VerifyUserDb();
             System.Diagnostics.Debug.WriteLine("> calling start");
