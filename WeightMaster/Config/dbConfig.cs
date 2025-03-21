5016d556674615a47e24952d6005e2fb0c11ca17
Office <Office@DESKTOP-3680TA8>
2025-03-20 22:39:22 -0700

    fixes

--- WeightMaster/Config/dbConfig.cs
+++ WeightMaster/Config/dbConfig.cs
@@ -14,7 +14,7 @@ namespace WeightMaster.Config
         {
             if (!optionsBuilder.IsConfigured)
             {
-                var connectionString = "Server=localhost;Database=weighthandlerdb;User=root;Password=root";
+                var connectionString = "Server=192.168.1.162;Database=weighthandlerdb;User=user;Password=password;"; 
                 optionsBuilder.UseMySql(connectionString, ServerVersion.Parse("8.0.25"));
             }
         }
