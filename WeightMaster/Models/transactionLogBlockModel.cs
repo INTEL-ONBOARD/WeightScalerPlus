b748a3f4aa65d50dbc0e3b71b0cde4bd190dbc1a
Office <Office@DESKTOP-3680TA8>
2025-03-22 09:45:55 -0700

    fixes: ongoing changes

--- WeightMaster/Models/transactionLogBlockModel.cs
+++ WeightMaster/Models/transactionLogBlockModel.cs
@@ -6,33 +6,37 @@ namespace WeightMaster.Models
 {
     public class TransactionLogBlockModel
     {
-
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string? LineName { get; set; }
-        public string? TransportAgent { get; set; }
-        public string? Company { get; set; }
-        public string? LeafWeightOfficer { get; set; }
-        public string? Supervisor { get; set; }
-        public string? BarcodeDetails { get; set; }
-        public string? NameWithInitials { get; set; }
-        public string? PhoneNumber { get; set; }
-        public string? Date { get; set; }
-        public int BoxCount { get; set; }
-        public int BagCount { get; set; }
-        public int MaximumNormalLeafWeight { get; set; }
-        public int TotalLeafWeight { get; set; }
-        public int ActualNormalLeafWeight { get; set; }
-        public int TotalGoldLeafWeight { get; set; }
-        public int Water { get; set; }
-        public int Morapuwata { get; set; }
-        public int Thambimata { get; set; }
-        public int Reject { get; set; }
-        public int BoxWeight { get; set; }
-        public int FinalGreenLeafCount { get; set; }
-        public int FinalGoldLeafCount { get; set; }
-        public double RealValue { get; set; }
 
+        public string? linename { get; set; }
+        public string? transportagent { get; set; }
+        public string? company { get; set; }
+        public string? leaf_weight_officer { get; set; }
+        public string? superviosr { get; set; } // Corrected based on your JSON
+
+        public string? barcode_details { get; set; }
+        public string? name_with_initials { get; set; }
+        public string? phone_number { get; set; }
+        public string? date { get; set; } // Assuming this should remain as string for format "YYYY-MM-DD"
+
+        public int box_count { get; set; }
+        public int bag_count { get; set; }
+        public double real_value { get; set; }
+
+        public int maximum_nomal_leaf_weight { get; set; }
+        public int total_leaf_weight { get; set; }
+        public int actual_nomal_leaf_weight { get; set; }
+        public int total_gold_leaf_weight { get; set; }
+
+        public int water { get; set; }
+        public int morapuwata { get; set; }
+        public int thambimata { get; set; }
+        public int reject { get; set; }
+        public int box_weight { get; set; }
+
+        public int final_green_leaf_count { get; set; }
+        public int final_gold_leaf_count { get; set; }
     }
 }
