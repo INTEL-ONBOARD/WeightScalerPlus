42c5737ce64240c665058f5d336be2dc31e39b8b
Office <Office@DESKTOP-3680TA8>
2025-03-31 00:12:34 +0530

    fixes:  UI fixes and sound logic added and completed

--- WeightMaster/Interfaces/MainWindow.xaml.cs
+++ WeightMaster/Interfaces/MainWindow.xaml.cs
@@ -544,6 +544,7 @@ namespace WeightMaster
             {
                 customerWindow.Close();
             }
+            runtimeService.KillRunExe();
             this.Close();
         }
 
@@ -563,7 +564,7 @@ namespace WeightMaster
                     {
                         foreach (var lineMaster in lineMasterData)
                         {
-                            System.Diagnostics.Debug.WriteLine($"ID: {lineMaster.id}, Line Name: {lineMaster.LineName}, Line Master: {lineMaster.LineMaster}");
+                            //System.Diagnostics.Debug.WriteLine($"ID: {lineMaster.id}, Line Name: {lineMaster.LineName}, Line Master: {lineMaster.LineMaster}");
                             lineNameCmb_st1.Items.Add(lineMaster.LineName);
                             lineNameCmb_st2.Items.Add(lineMaster.LineName);
                         }
@@ -590,7 +591,6 @@ namespace WeightMaster
                     {
                         foreach (string supervisor in supervisorData)
                         {
-                            System.Diagnostics.Debug.WriteLine($"supervisor Name: {supervisor}");
                             supervisorCmb_st1.Items.Add(supervisor);
                             supervisorCmb_st2.Items.Add(supervisor);
                         }
@@ -833,7 +833,9 @@ namespace WeightMaster
                 customerWindow.Close();
             }
             //hmm you need either to clear all textboxes or restart the app.
+            System.Diagnostics.Debug.WriteLine("system check 1");
             runtimeService.KillRunExe();
+            System.Diagnostics.Debug.WriteLine("system check 2");
             StartupTheAppAsync();
         }
 
@@ -1651,6 +1653,7 @@ namespace WeightMaster
                 //            currentAcceptedSackWeightTxt_st2 = CeilingValue;
 
                 //to catch up with Enter key press event(in case of the manual click)
+                ConsoleSound.PlayStable();
             }
             else
             {
@@ -2408,7 +2411,7 @@ namespace WeightMaster
                     {
                         statusLabel.Content = "Verifying Database Status(3)...";
                     });
-                    //await _consoleHandler.verifyMemberDb();
+                    await _consoleHandler.verifyMemberDb();
                     Dispatcher.Invoke(() =>
                     {
                         statusLabel.Content = "DB Verified(3)";
--- WeightMaster/Services/runtime.cs
+++ WeightMaster/Services/runtime.cs
@@ -158,26 +158,26 @@ namespace WeightMaster.Services
         {
             try
             {
-                System.Diagnostics.Debug.WriteLine("Attempting to open file...");
+                // System.Diagnostics.Debug.WriteLine("Attempting to open file...");
 
                 using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
                 {
-                    System.Diagnostics.Debug.WriteLine("File opened successfully.");
+                    // System.Diagnostics.Debug.WriteLine("File opened successfully.");
 
                     using (var reader = new StreamReader(stream))
                     {
-                        System.Diagnostics.Debug.WriteLine("Reading file...");
+                        // System.Diagnostics.Debug.WriteLine("Reading file...");
 
                         string json = reader.ReadToEnd();
-                        System.Diagnostics.Debug.WriteLine("File content read successfully.");
+                        // System.Diagnostics.Debug.WriteLine("File content read successfully.");
 
                         var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
                         var data = JsonSerializer.Deserialize<WeightData>(json, options);
 
                         if (data != null)
                         {
-                            System.Diagnostics.Debug.WriteLine($"> Value: {data.Value}, Stable: {data.Stable}");
-                             window.weightScalerValTxt_st1.Text = data.Value.ToUpper().Replace("KG", "").Trim();
+                            // System.Diagnostics.Debug.WriteLine($"> Value: {data.Value}, Stable: {data.Stable}");
+                            window.weightScalerValTxt_st1.Text = data.Value.ToUpper().Replace("KG", "").Trim();
                              window.weightScalerValTxt_st2.Text = data.Value.ToUpper().Replace("KG", "").Trim();
 
                             if (data.Stable.Equals("true"))
