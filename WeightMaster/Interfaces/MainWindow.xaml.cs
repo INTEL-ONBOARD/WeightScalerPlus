a6a49fe761d4181aa46c7f2118e233611664502b
B1N4L <163591915+B1N4L@users.noreply.github.com>
2025-04-06 17:28:04 +0530

    fix: missing MainWindow.xaml

--- WeightMaster/Interfaces/MainWindow.xaml.cs
+++ WeightMaster/Interfaces/MainWindow.xaml.cs
@@ -2334,7 +2334,7 @@ namespace WeightMaster
             //    MessageBox.Show("manually changing steps");
             //    currentStep_st2 = 5;
             //}
-            MessageBox.Show("round confirmed!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            //MessageBox.Show("round confirmed!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
             bool hasfailed = false;
             if (hasfailed)
             {
