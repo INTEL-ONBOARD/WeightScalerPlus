6c024ac55319bef0354e5245523b89be255e7f9c
kkwenuja <kkwenuja@KKWENUJAF103>
2025-06-04 05:38:07 +0530

    fixes

--- WeightMaster/Config/ApiClient.cs
+++ WeightMaster/Config/ApiClient.cs
@@ -1,36 +1,4 @@
-﻿//using System;
-//using System.Net.Http;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Newtonsoft.Json;
-
-//public class ApiClient
-//{
-//    private readonly HttpClient _httpClient;
-
-//    public ApiClient()
-//    {
-//        _httpClient = new HttpClient();
-//    }
-
-//    public async Task<T> GetAsync<T>(string url)
-//    {
-//        HttpResponseMessage response = await _httpClient.GetAsync(url);
-//        response.EnsureSuccessStatusCode();
-//        string responseData = await response.Content.ReadAsStringAsync();
-//        return JsonConvert.DeserializeObject<T>(responseData);
-//    }
-
-//    public async Task<T> PostAsync<T>(string url, object data)
-//    {
-//        string jsonData = JsonConvert.SerializeObject(data);
-//        StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-//        HttpResponseMessage response = await _httpClient.PostAsync(url, content);
-//        response.EnsureSuccessStatusCode();
-//        string responseData = await response.Content.ReadAsStringAsync();
-//        return JsonConvert.DeserializeObject<T>(responseData);
-//    }
-//}
+﻿
 
 using System;
 using System.Net.Http;
