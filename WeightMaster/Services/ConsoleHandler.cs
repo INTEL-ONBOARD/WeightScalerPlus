762024e30848e8a55eb9de206e3b7e0120dc77e2
kkwenuja <kkwenuja@KKWENUJAF103>
2025-06-04 16:03:58 +0530

    fixes

--- WeightMaster/Services/ConsoleHandler.cs
+++ WeightMaster/Services/ConsoleHandler.cs
@@ -160,7 +160,7 @@ namespace WeightMaster.Services
         {
             return await _engine.getLatestPost();
         }
-        public async Task<List<GreenLeafPostModel>> GetRecent(string memberid,string date_)
+        public async Task<List<GreenLeafPostModel>> getDatabyMemberiDandDate(string memberid,string date_)
         {
             return await _engine.getPostsByMemberAndDate(memberid,date_);
         }
