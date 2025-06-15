3dbae627733d6b687b4264f9b451f0e36c641279
B1N4L <163591915+B1N4L@users.noreply.github.com>
2025-06-15 10:00:47 +0530

    update: topmost and internet connection

--- WeightMaster/Interfaces/MainWindow.xaml.cs
+++ WeightMaster/Interfaces/MainWindow.xaml.cs
@@ -3950,7 +3950,7 @@ namespace WeightMaster
             bool failed = false;
 
             // Check internet connection
-            /*try
+            try
             {
                 Dispatcher.Invoke(() =>
                 {
@@ -3994,7 +3994,7 @@ namespace WeightMaster
                     statusLabel.Content = $"Error checking internet: {ex.Message}";
                 });
                 return;
-            }*/
+            }
 
             // Repeat until no exception occurs
             bool firstItr = true; //to check the first iteration has passed(to show different status label content after first iteration)
